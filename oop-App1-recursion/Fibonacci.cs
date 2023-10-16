df5c802d77b3d19b7de1d85b1b04cd292a91a32a
cmandea <146120390+cmandea@users.noreply.github.com>
2023-10-16 14:00:46 +0300

    v1

---
+++ oop-App1-recursion/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Reflection.Metadata.Ecma335;
+
+namespace oop_App1_recursion
+{
+    internal class Fibonacci
+    {
+        public static string FibonacciRec
+         (
+          int number1,
+          int number2,
+          int i,
+          int sizeNumber
+         )
+        {
+            string returnString="";
+            if (i <= sizeNumber)
+            {
+                //Console.Write("{0} ", firstnumber);
+                 returnString = number1.ToString();
+
+                returnString = returnString + " " + FibonacciRec(number2, number1 + number2, i + 1, sizeNumber);
+            }
+            return returnString;
+        }
+    }
+}
---
+++ oop-App1-recursion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace oop_App1_recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = ArrayHelper.ReadNumber("The size of array=", 3, 0);
+
+           // int nrforOccurrance = ArrayHelper.ReadNumber("Please enter number for count of occurrance=", 3, 0);
+            int[] array = new int[size];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int item = ArrayHelper.ReadNumber($"Element[{i}]=", 3, 0);
+                array[i] = item;
+            }
+            ArrayHelper.PrintArray("Array=", array);
+
+            if (array != null)
+            {
+                
+                
+                Console.WriteLine($"Min of the array =[{string.Join(",",array)}] is: "+ArrayHelper.readMinArray(array, 0, array[0]));
+                Console.WriteLine($"Max of the array =[{string.Join(",", array)}] is: " + ArrayHelper.readMaxArray(array, 0, array[0]));
+               
+            }
+
+            int sizeNumber = ArrayHelper.ReadNumber("The size of number of Fibonacci=", 3, 0);
+            Console.WriteLine($"Recursion String of Fibonacci= " + Fibonacci.FibonacciRecursive(0, 1, 1, sizeNumber));
+            
+
+        }
+    }
+}
